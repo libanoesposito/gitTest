4a89ee34a9af16edd5130cfe59ecdbb91a07b8b9
libano <libanoesposito@gmail.com>
2026-01-12 10:35:47 +0100

    Abbiamo aggiunto i sopravvisuti

--- src/testGit/Avvio.java
+++ src/testGit/Avvio.java
@@ -4,6 +4,8 @@ public class Avvio {
 
 	public static void main(String[] args) {
 		System.out.println("toto sopravvisuti");
+		System.out.println("Il buon Mattia");
+		System.out.println("la mistica Francesca");
 	}
 
 }
